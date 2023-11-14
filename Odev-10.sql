0bb3007190c573b939cfee79c0cf0eb34e740dbb
halecosar <halealtunakar@hotmail.com>
2023-11-14 23:37:39 +0300

    Create Odev-10.sql

---
+++ Odev-10.sql
@@ -0,0 +1,14 @@
+--1-
+SELECT c.city, co.country FROM city AS c
+LEFT JOIN country AS co ON c.country_id = co.country_id
+--2-
+SELECT p.payment_id,c.first_name, c.last_name FROM payment AS p
+RIGHT JOIN customer AS c ON c.customer_id = p.customer_id
+--3-
+SELECT r.rental_id, c.first_name, c.last_name FROM rental AS r
+FULL JOIN customer AS c ON c.customer_id = r.customer_id
+
+ 
+
+
+
