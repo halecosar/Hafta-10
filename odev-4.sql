26f943f64a223d3f367c61a96fae02b621080dbf
halecosar <halealtunakar@hotmail.com>
2023-11-14 12:25:06 +0300

    Create odev-4.sql

---
+++ odev-4.sql
@@ -0,0 +1,21 @@
+--1-
+SELECT DISTINCT replacement_cost FROM film;
+
+--2-
+SELECT COUNT (replacement_cost) FROM film;
+
+--3-
+SELECT COUNT (title) FROM film WHERE title LIKE 'T%' AND rating ='G'
+
+--4-
+SELECT COUNT (country) FROM country WHERE country LIKE '_____'
+
+--5-
+SELECT COUNT (city) FROM CITY WHERE city ILIKE '%n' 
+
+
+
+
+
+
+
