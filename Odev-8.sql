fb4d69a1d06ee1d846ad54c46e2bc8bb0a848f55
halecosar <halealtunakar@hotmail.com>
2023-11-14 19:44:31 +0300

    Create Odev-8.sql

---
+++ Odev-8.sql
@@ -0,0 +1,13 @@
+UPDATE employee SET name='HALE' WHERE ID=93
+UPDATE employee SET birthday='1997-10-27' WHERE ID=73
+UPDATE employee SET mail='halecosar@gmail.com' WHERE NAME='Cilka'
+UPDATE employee SET name='Milka' WHERE birthday='1982-09-25'
+UPDATE employee SET mail='jspensley17@etsy.com' WHERE birthday='1984-08-27'
+
+DELETE FROM employee WHERE name='Silvia'
+DELETE FROM employee WHERE birthday='1995-12-16'
+DELETE FROM employee WHERE mail='gduddle1z@wix.com'
+DELETE FROM employee WHERE ID=1
+DELETE FROM employee WHERE name='HALE'
+
+
